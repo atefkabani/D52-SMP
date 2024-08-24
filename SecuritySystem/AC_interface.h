ce0cde4d6b2bca617a2d2c995a6cd175a624d73e
Atef Elkabbani <atefkabani@gmail.com>
2024-08-24 20:58:43 +0300

    Add files via upload

---
+++ SecuritySystem/AC_interface.h
@@ -0,0 +1,18 @@
+/*
+ * AC_interface.h
+ *
+ * Created: 7/5/2024 12:54:31 PM
+ *Author :  D52
+ */ 
+
+
+#ifndef AC_INTERFACE_H_
+#define AC_INTERFACE_H_
+
+void AC_voidInit  (void);
+void AC_voidShowMenuKPD(void);
+void AC_voidShowMenuBluetooth(void);
+
+
+
+#endif /* AC_INTERFACE_H_ */
